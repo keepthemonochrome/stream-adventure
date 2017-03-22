3547e4f216d34c7a77074e6928aad1ffffffb4da
Hong <keepthemonochrome@gmail.com>
2017-03-21 22:02:32 -0700

    Add notes

--- problems/lines/problem.js
+++ problems/lines/problem.js
@@ -1,29 +1,23 @@
 var split = require('split');
 var through = require('through2');
 
-// process.stdin
-//   .pipe(split())
-//   // .pipe(through(function(chunk, enc, callback){
-//   //   var newchunk = chunk.toString().toLowerCase();
-//   //   // console.log
-//   //   this.push(newchunk);
-//   //   callback();
-//   //   // console.log(chunk)
-//   // }))
-//   .pipe(process.stdout);
 var flag = true;
-var split = require('split');
-  process.stdin
-      .pipe(split(''))
-      // .pipe()
-      .pipe(through(function (line, _, next) {
-          if(flag){
-            console.log(line.toString().toLowerCase());
-          } else {
-            console.log(line.toString().toUpperCase());
-          }
-          flag = !flag;
-          // console.log(line.toString());
-          next();
-      }))
-  ;
+process.stdin
+    .pipe(split(''))
+    .pipe(through(function (chunk, enc, callback) {
+        if(flag){
+          console.log(chunk.toString().toLowerCase());
+          // console.dir(chunk.toString().toLowerCase());
+
+        } else {
+          console.log(chunk.toString().toUpperCase());
+          // console.dir(chunk.toString().toUpperCase());
+        }
+        flag = !flag;
+        // console.log(line.toString());
+        callback();
+    }));
+
+
+// notes
+// - what does console.dir do?

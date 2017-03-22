a2f657a0f6844f8974edf377d2c93de3e8dee2d9
Hong <keepthemonochrome@gmail.com>
2017-03-21 22:34:15 -0700

    (ref-sol) Cannot find any document about stream.write!!!

---
+++ problems/websockets/problem.js
@@ -0,0 +1,15 @@
+// var ws = require('websocket-stream');
+// var through = require('through2');
+
+// var stream = ws('ws://localhost:8099');
+// // ws.send('hello\n');
+// // "hello\n".pipe(stream);
+// stream.pipe(through((buf, _, next) => {
+//   this.push("hello\n");
+//   next();
+// }, (done) => done()));
+
+var ws = require('websocket-stream');
+
+var stream = ws('ws://localhost:8099');
+stream.write('hello\n'); //???? Cannot find anywhere in the doc

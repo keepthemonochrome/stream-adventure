a4147dbaa7777c321c15933cb6338a12c7d69d0f
Hong <keepthemonochrome@gmail.com>
2017-03-28 17:31:18 -0700

    (fix) through(write, end) -- end function for the last JSON object & JSON.stringify(buffer)

--- problems/combiner/program.js
+++ problems/combiner/program.js
@@ -3,28 +3,33 @@ var split = require('split');
 var through = require('through2');
 var zlib = require('zlib');
 var gzip = zlib.createGzip();
-    module.exports = function () {
-      var temp = {};
-      var splitted = split();
-      var json = through(function(buf, _, next){
 
-        if(buf.type === "genre"){
-          this.push(temp);
+module.exports = function () {
+  var temp = {};
+  var splitted = split();
+  var json = through(
+    function write(buf, _, next){
+      buf = JSON.parse(buf);
+      if(buf.type === "genre"){
+        if(Object.keys(temp).length !== 0) this.push(JSON.stringify(temp)+'\n');
           temp = {"name":buf.name, "books":[]};
-        } else if(buf.type === "book"){
-          temp["books"].push(buf.name);
-        }
-        next();
+      } else if(buf.type === "book"){
+        temp["books"].push(buf.name);
       }
-    // , function end(next){ this.push(temp + '\n'); next();}
+      next();
+    }
+  , function end(next){ // end function to push the last remaining temp obj
+      if(Object.keys(temp).length !== 0) this.push(JSON.stringify(temp) + '\n');
+      next();
+    }
   );
 
-        return combine(
-            // read newline-separated json,
-            splitted,
-            // group books into genres,
-            json,
-            // then gzip the output
-            gzip
-        )
-    }
+  return combine(
+      // read newline-separated json,
+      splitted,
+      // group books into genres,
+      json,
+      // then gzip the output
+      gzip
+  )
+}

8a69c91dcbc491196d28de7aff4bceb94a45c9e9
Hong <keepthemonochrome@gmail.com>
2017-03-28 17:42:06 -0700

    (fix) Modularize, convert to es6

--- problems/combiner/program.js
+++ problems/combiner/program.js
@@ -1,35 +1,33 @@
-var combine = require('stream-combiner2')
-var split = require('split');
-var through = require('through2');
-var zlib = require('zlib');
-var gzip = zlib.createGzip();
+const combine = require('stream-combiner2')
+const split = require('split');
+const through = require('through2');
+const zlib = require('zlib');
 
 module.exports = function () {
-  var temp = {};
-  var splitted = split();
-  var json = through(
-    function write(buf, _, next){
-      buf = JSON.parse(buf);
-      if(buf.type === "genre"){
-        if(Object.keys(temp).length !== 0) this.push(JSON.stringify(temp)+'\n');
-          temp = {"name":buf.name, "books":[]};
-      } else if(buf.type === "book"){
-        temp["books"].push(buf.name);
-      }
-      next();
+  let category = {};
+  // catergorize json objs by genre
+  const write = function (buf, _, next){
+    const line = JSON.parse(buf);
+    if(line.type === "genre"){
+      if(Object.keys(category).length !== 0) this.push(JSON.stringify(category)+'\n');
+        category = {name:line.name, books:[]};
+    } else if(line.type === "book"){
+      category["books"].push(line.name);
     }
-  , function end(next){ // end function to push the last remaining temp obj
-      if(Object.keys(temp).length !== 0) this.push(JSON.stringify(temp) + '\n');
+    next();
+  }
+  // end function to push the last remaining category obj
+  const end = function (next){
+      if(Object.keys(category).length !== 0) this.push(JSON.stringify(category) + '\n');
       next();
-    }
-  );
+  }
 
   return combine(
       // read newline-separated json,
-      splitted,
+      split(),
       // group books into genres,
-      json,
+      through(write, end),
       // then gzip the output
-      gzip
+      zlib.createGzip()
   )
 }
